be1e1e770571b97999eacc48c2939c771bb31c29
Shoxrux Nigmatillayev <shoxrux.nigmatillayev@ucell.uz>
2025-11-11 11:03:44 +0500

    Add Query parameters data validation

--- books2.py
+++ books2.py
@@ -1,4 +1,4 @@
-from fastapi import Body, FastAPI, Path
+from fastapi import Body, FastAPI, Path, Query
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -21,6 +21,7 @@ class Book:
         self.rating = rating
         self.published_date = published_date
 
+
 class BookRequest(BaseModel):
     id: Optional[int] = Field(description="ID is not needed on create", default=None)
     title: str = Field(min_length=3)
@@ -41,6 +42,7 @@ class BookRequest(BaseModel):
         }
     }
 
+
 BOOKS = [
     Book(1, "Computer Science Pro", "codingwithroby", "A very nice book!", 5, 2016),
     Book(2, "Be fast with FastAPI", "codingwithroby", "A great book!", 5, 2016),
@@ -55,6 +57,7 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
+
 @app.get("/books/{published_year}", tags=["Fetching Data"])
 async def get_book_by_year(published_year: int = Path(lt=2026)):
     books_to_return = []
@@ -63,19 +66,22 @@ async def get_book_by_year(published_year: int = Path(lt=2026)):
             books_to_return.append(book)
     return books_to_return
 
+
 @app.get("/books/{book_id}/", tags=["Fetching Data"])
-async def read_book(book_id: int=  Path(gt=0)):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
 
+
 @app.get("/books/", tags=["Fetching Data"])
-async def get_book_by_rating(book_rating: int):
+async def get_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
             books_to_return.append(book)
     return books_to_return
+
 
 @app.post("/create-book", tags=["Creating Data"])
 async def create_book(book_request: BookRequest):
@@ -83,6 +89,7 @@ async def create_book(book_request: BookRequest):
     # Book(**book_request.dict()) = Book(**book_request.model_dump())
     new_book = Book(**book_request.dict())
     BOOKS.append(find_book_id(new_book))
+
 
 # This method helps us to auto increment the id of the books that are being added
 def find_book_id(book: Book):
@@ -93,11 +100,13 @@ def find_book_id(book: Book):
 
     return book
 
+
 @app.put("/books/update_book", tags=["Updating Data"])
 async def update_book(book: BookRequest):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+
 
 @app.delete("/books/{book_id}", tags=["Deleting Data"])
 async def delete_book(book_id: int = Path(gt=0)):
